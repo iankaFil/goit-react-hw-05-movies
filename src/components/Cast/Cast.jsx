fa003d149e441be673041da930c4eb9cbc7b2c0d
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-01-29 13:17:23 -0700

    ready

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -28,20 +28,23 @@ const Cast = () => {
       {loading ? (
         'Loading...'
       ) : data && data.length > 0 ? (
-        <ul className={css.list}>
-          {data.map(({ name, profile_path, id }) => (
-            <li key={id} className={css.listItem}>
-              <p className={css.listDescr}>{name}</p>
-              {profile_path && (
-                <img
-                  className={css.listImg}
-                  alt={name}
-                  src={`https://image.tmdb.org/t/p/w92${profile_path}`}
-                />
-              )}
-            </li>
-          ))}
-        </ul>
+        <div className={css.listWrap}>
+          <ul className={css.list}>
+            {data.map(({ name, character, profile_path, id }) => (
+              <li key={id} className={css.listItem}>
+                {profile_path && (
+                  <img
+                    className={css.listImg}
+                    alt={name}
+                    src={`https://image.tmdb.org/t/p/w92${profile_path}`}
+                  />
+                )}
+                <p className={css.listName}>{name}</p>
+                <p className={css.listCharacter}>{character}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
         <p className={css.error}>No data found</p>
       )}
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -30,16 +30,18 @@ const Reviews = () => {
       {loading ? (
         'Loading...'
       ) : data && data.length > 0 ? (
-        <ul className={css.list}>
-          {data.map(({ author, content, id }) => (
-            <li key={id} className={css.listItem}>
-              <p className={css.listDescr}>{author}</p>
-              {content && content}
-            </li>
-          ))}
-        </ul>
+        <div className={css.listWrap}>
+          <ul className={css.list}>
+            {data.map(({ author, content, id }) => (
+              <li key={id} className={css.listItem}>
+                <p className={css.listName}>{author}</p>
+                {content && content}
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
-        <p>No reviews found</p>
+        <p className={css.error}>No reviews found</p>
       )}
     </>
   );
